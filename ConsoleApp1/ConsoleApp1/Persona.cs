fcb524f2ab54869ad32ec5ef152ae5e714d1750e
CarlosAkel <cmakel@miuandes.cl>
2020-03-17 16:38:50 -0300

    work

---
+++ ConsoleApp1/ConsoleApp1/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Persona
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int min { get; set; }
+        public int max { get; set; }
+
+        public int Lanzar()
+        {
+            Random Roll = new Random();
+            int Resp;
+            Resp = Roll.Next(0, 3);
+            return Resp;
+        }
+
+        public Persona(string Name, string LastName)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+
+        }
+        public string MyName()
+        {
+            return $"{Name}-{LastName}";
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Yo = new Persona("Bob", "Kunga");
+            Console.WriteLine(Yo.Lanzar());
+            Console.WriteLine(Yo.MyName());
+
+
+        }
+
+
+    }
+}
